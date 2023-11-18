fcd4e20de8fc9879c54abf9cc20bf5201cbf9c8b
Chimder <127688661+Chimder@users.noreply.github.com>
2023-11-18 16:19:05 +0200

    some media

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,15 @@
 import clsx from "clsx";
-import React from "react";
+import React, { useState } from "react";
 import { LuLogIn } from "react-icons/lu";
 import { Link } from "react-router-dom";
 import s from "./Header.module.scss";
 
 export const Header = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
   return (
     <header className={s.header}>
       <div className={s.container}>
@@ -23,6 +28,20 @@ export const Header = () => {
           <div className={clsx(s.sing_in, s.header_word)}>SING IN</div>
           <LuLogIn />
         </div>
+        <div className={s.mobile_toggle} onClick={toggleMenu}>
+          {isMenuOpen ? "✕" : "≡"}
+        </div>
+        {isMenuOpen && (
+          <div className={s.mobile_container}>
+            <div className={s.header_word_mob}>NEWS</div>
+            <div className={s.header_word_mob}>ABOUT</div>
+            <div className={s.header_word_mob}>WATCH</div>
+            <div className={s.right_mob}>
+              <div className={s.sing_in_mob}>SING IN</div>
+              <LuLogIn />
+            </div>
+          </div>
+        )}
       </div>
     </header>
   );
